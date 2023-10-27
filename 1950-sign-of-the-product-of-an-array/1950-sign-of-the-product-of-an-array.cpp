0890fce4920087bf3bf89bfe11f31f186f73373b
AL NAFIS FUAD SHUVO <88796714+shuvonafis66@users.noreply.github.com>
2023-10-27 22:41:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (54.92%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negative_count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            return 0;
+            else if(nums[i]<0)
+            {
+            negative_count++;
+            }
+        }
+    return negative_count%2==0?1:-1;
+    }
+};
