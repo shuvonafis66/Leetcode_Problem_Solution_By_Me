39285251a388ea7c88fcf62e59eda7ff11c34f9b
AL NAFIS FUAD SHUVO <88796714+shuvonafis66@users.noreply.github.com>
2023-10-27 22:26:39 +0530

    Time: 2430 ms (5.09%), Space: 72.7 MB (7.09%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,12 +1,10 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<int> replaceElements(std::vector<int>& arr) {
-        std::vector<int> ans;
-        for (int i = 0; i < arr.size() - 1; i++) {
-            ans.push_back(*std::max_element(arr.begin() + i + 1, arr.end()));
+    vector<int> replaceElements(vector<int>& arr) {
+        vector<int>ans;
+        for(int i=0;i<arr.size()-1;i++)
+        {
+        ans.push_back(*max_element(arr.begin()+i+1,arr.end()));
         }
         ans.push_back(-1);
         return ans;
